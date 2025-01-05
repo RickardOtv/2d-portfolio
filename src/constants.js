a38210fb215ca76cc9d31b5f67b1e967329f573a
Rickard Otvos <s2106051@student.hb.se>
2025-01-05 15:45:00 +0100

    First commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript and Java these days.
+        I also like C# and Python. Anyway regardless of the language, I just like programming.
+        Here is my <a href="https://github.com/RickardOtv" target="_blank">Github</a>!`,
+    "cs-degree": `This is my degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.
+    You can also check it out on my <a href="www.linkedin.com/in/rickard-ötvös" target="_blank"> LinkedIn</a>.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
+    If you want to learn how I built this portfolio you're currently playing through!. Go to <a href="https://youtube.com/@jslegenddev" target="_blank">this channel</a>! (If you like it)`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
+    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. Want to check it out on my <a href="www.linkedin.com/in/rickard-ötvös" target="_blank"> LinkedIn?</a>`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+    Kaboom.js by watching some tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+    library: `There are a lot of programming books on my shelves.
+    I probably only read one of them. Do you also compulsively buy technical books without ever finishing them?`,
+    exit: `If you want to exit my portfolio, just close the tab.`,
+  };
